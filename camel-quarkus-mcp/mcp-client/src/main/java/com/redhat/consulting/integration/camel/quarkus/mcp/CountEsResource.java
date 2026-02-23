0ac5e840152ae83e7be8f61f3687decd15e2c758
Keith Herbert <kherbert@kherbert-thinkpadt14sgen2i.glenmso.csb>
2026-02-23 16:16:02 -0500

    Add logging to camel-quarkus-mcp/mcp-client.

--- camel-quarkus-mcp/mcp-client/src/main/java/com/redhat/consulting/integration/camel/quarkus/mcp/CountEsResource.java
+++ camel-quarkus-mcp/mcp-client/src/main/java/com/redhat/consulting/integration/camel/quarkus/mcp/CountEsResource.java
@@ -1,5 +1,6 @@
 package com.redhat.consulting.integration.camel.quarkus.mcp;
 
+import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -16,6 +17,9 @@ public class CountEsResource {
     @Path("/{word}")
     @Produces(MediaType.TEXT_PLAIN)
     public String ask(@PathParam("word") String word) {
-        return aiLetterCounterService.countEs(word);
+        Log.infof("Counting 'e's in %s", word);
+        String result = aiLetterCounterService.countEs(word);
+        Log.infof("Result=%s", result);
+        return result;
     }
 }
